336d3c9430d313dce2dc836ad4ca7c80baabcbef
Yousaf Khan <Yousafkhan.me@outlook.com>
2024-09-17 17:22:01 +0500

    fixed controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,30 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
+    # No need for anything in here, we are just going to render our
+    # new.html.erb AKA the login page
   end
 
   def create
-    @user = User.find_by(name: params[:user][:name])
+    # Look up User in db by the email address submitted to the login form and
+    # convert to lowercase to match email in db in case they had caps lock on:
+    user = User.find_by(email: params[:login][:email].downcase)
 
-    if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to root_path
+    # Verify user exists in db and run has_secure_password's .authenticate()
+    # method to see if the password submitted on the login form was correct:
+    if user && user.authenticate(params[:login][:password])
+      # Save the user.id in that user's session cookie:
+      session[:user_id] = user.id.to_s
+      redirect_to root_path, notice: 'Successfully logged in!'
     else
-      flash[:alert] = 'Login failed'
-      redirect_to new_session_path
+      # if email or password incorrect, re-render login page:
+      flash.now.alert = 'Incorrect email or password, try again.'
+      render :new
     end
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_path
+    # delete the saved user_id key/value from the cookie:
+    session.delete(:user_id)
+    redirect_to login_path, notice: 'Logged out!'
   end
 end
