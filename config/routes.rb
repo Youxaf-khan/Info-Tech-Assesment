a18c555a682048e488314e269ab2c10433aa1173
Yousaf Khan <Yousafkhan.me@outlook.com>
2024-09-17 16:53:47 +0500

    Intial commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  root to: 'users#new'
+
+  get 'users/new' => 'users#new', as: :new_user
+  post 'users' => 'users#create'
+
+  # ----- add these lines here: -----
+
+  # log in page with form:
+  get '/login'     => 'sessions#new'
+
+  # create (post) action for when log in form is submitted:
+  post '/login'    => 'sessions#create'
+
+  # delete action to log out:
+  delete '/logout' => 'sessions#destroy'
+end
