a1e8e0ed0c3ab373ba01efed7636470171304123
Yousaf Khan <Yousafkhan.me@outlook.com>
2024-09-17 17:12:33 +0500

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/secret'
-  resources :user_sessions, only: %i[new create destroy]
+  resources :sessions, only: %i[new create destroy]
   resources :users, only: %i[index new create]
 end
